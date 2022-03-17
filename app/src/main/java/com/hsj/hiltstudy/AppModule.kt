4aaa29e8ac0d335799a3120ac816d8fe65d7b438
JoHongsik <queue1991@naver.com>
2022-03-17 14:55:52 +0900

    String Inject Test

---
+++ app/src/main/java/com/hsj/hiltstudy/AppModule.kt
@@ -0,0 +1,18 @@
+package com.hsj.hiltstudy
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideTestString() = "This is a string we will inject"
+
+
+}
--- app/src/main/java/com/hsj/hiltstudy/MainActivity.kt
+++ app/src/main/java/com/hsj/hiltstudy/MainActivity.kt
@@ -2,10 +2,20 @@ package com.hsj.hiltstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var testString : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity","Test String from MainActivity : $testString")
     }
 }
